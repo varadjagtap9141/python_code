faf0b329fc098153b6ea999d3f1301641f35d0c2
Varad Jagtap <varadjagtap31@gmail.com>
2025-11-06 23:33:35 +0530

    update

---
+++ break_continue.py
@@ -0,0 +1,19 @@
+# for i in range(10):
+#     if i == 5:
+#         break
+#     print(i)
+
+
+
+# continue example
+# for i in range(50):
+#     if i % 2 == 0:
+#         continue
+#     print(i)
+
+# star pattern - triangle
+
+for i in range(5):
+    for j in range(i + 1):
+        print("*", end=" ")
+    print()
